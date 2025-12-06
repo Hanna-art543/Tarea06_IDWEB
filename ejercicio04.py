515fe96f410d50d316b87e111f3be1e347a86b97
Ana Miranda <amirandalo@unsa.edu.pe>
2025-12-06 06:23:07 -0500

    Subir ejercicio 04

---
+++ ejercicio04.py
@@ -0,0 +1,18 @@
+def ingresar_numero():
+    while True:
+        try:
+            num = int(input("Ingrese un número entero: "))
+            cuadrado = num ** 2
+            print(f"Número: {num} | Cuadrado: {cuadrado}")
+            break  
+
+        except ValueError:
+            print("Error: debes ingresar un número válido. Intenta otra vez.\n")
+
+
+def main():
+    print("Bienvenidos al programa para ingresar un número entero.")
+    ingresar_numero()
+
+
+main()
